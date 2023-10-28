da7e393a0f6840e594c6a3d3c11c8e71bde898e8
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2023-10-28 14:06:27 +1100

    submission ready

--- README.md
+++ README.md
@@ -1,53 +1,6 @@
 ## aqol6dmap_ss: Create the Scientific Manuscript For An AQoL-6D Utility Mapping Study
 This R Markdown subroutine program creates a scientific manuscript that summarises a utility mapping study. This subroutine is a study specific customisation of a utility mapping template manuscript authoring program available at https://doi.org/10.5281/zenodo.5976987.
 
-In order for this program to execute properly it must be supplied with a results summary object. The best way to do this is to call this program via the commands outlined in the Complete Study Program. The complete study program is available in the https://github.com/orygen/aqol6dmap_csp repository. A slightly more generalised version of that program is summarised in the CSP.pdf file available from: https://doi.org/10.7910/DVN/DKDIB0
-
-### Additional steps
-This current version of this program uses a template which will produce PDFs with figures and tables that render on top of each other. To fix this issue some additional code needs to be run.
-
-First, from your R console change your working directory to where the "Parent_PDF" directory is located on your local machine. Note you will have to replace "PATH_TO_SUBROUTINE" with the path to wherever you have downloaded this subroutine's parent directory ("aqol6dmap_ss"") to.
-
-```r
-setwd("PATH_TO_SUBROUTINE/Real/Markdown/Manuscript_Submission/Parent_PDF") # aqol6dmap_ss
-```
-
-Next, run the following commands in your R console to copy both the bibliography and LaTeX template to the "Parent_PDF" directory and to modify the parts of the template relating to tables and figures.
-
-```r
-file.copy("../Data/submissionRefs.bib","RefCopy.bib", overwrite = T)
-ready4::write_from_tmp("../../../Reports/Manuscript_Submission/Manuscript.tex",
-                       dest_paths_chr = "Manuscript.tex",
-                       edit_fn_ls = list(function(txt_chr,
-                                                  package_1L_chr){
-                         txt_chr <- purrr::map_chr(txt_chr,
-                                                   ~
-                                                     stringi::stri_replace_all_regex(.x,
-                                                                              "\\{table\\}",
-                                                                              "\\{table*\\}")
-                                                   ) %>%
-                           purrr::map_chr(~ stringi::stri_replace_all_regex(.x,
-                                                           "\\{figure\\}",
-                                                           "\\{figure*\\}"))
-                         # \counterwithin{figure*}{section}
-                         # \counterwithin{table*}{section}
-                         txt_chr
-                       })
-)
-
-```
-
-Now in a terminal session (ideally from the Terminal pane in RStudio) you need to update the bibliography cache.
-```
-biber Manusript
-```
-
-You can now open the  Manuscript.tex file in RStudio and click on the "Compile PDF button". That will generate a Manuscript with the figures and tables issue fixed.
-
-To copy the rendered PDF to the reports directory run the following command in your R console.
-
-```r
-file.copy("Manuscript.pdf","../../../Reports/Manuscript_Submission/Manuscript.pdf", overwrite = T)
-```
+In order for this program to execute properly it must be supplied with a results summary object. If you have access to a previously generated study results summary object, then follow the steps in SetUp.R. Alternatively, you can rerun the study analysis and call this manuscript authoring sub-routine by running the complete study replication program available in the https://github.com/orygen/aqol6dmap_csp repository. A slightly more generalised version of that program is summarised in the CSP.pdf file available from: https://doi.org/10.7910/DVN/DKDIB0
 
 For more general information about the framework used to develop this subroutine visit www.ready4-dev.com
--- SetUp.R
+++ SetUp.R
@@ -3,11 +3,14 @@ library(ready4show)
 #path_1L_chr <- "SPECIFY PATH ENDING IN '/' "
 A <- readRDS(paste0(path_1L_chr,"A.RDS")) # Read in results
 A@d_TTUReports@a_TTUSynopsis@a_Ready4showPaths@outp_data_dir_1L_chr <- paste0(path_1L_chr,"Real") # Update relative path to raw output
-A@d_TTUReports@a_TTUSynopsis@b_SpecificResults@a_SpecificShareable@shareable_outp_ls$results_ls$paths_to_figs_ls <- A@c_SpecificProject@c_SpecificResults@a_SpecificShareable@shareable_outp_ls$paths_to_figs_ls <- specific::make_paths_to_ms_smry_plts_ls(paste0(A@d_TTUReports@a_TTUSynopsis@a_Ready4showPaths@outp_data_dir_1L_chr, "/Output"), 
-                                                                                                                                                                                                                                                            A@c_SpecificProject@c_SpecificResults@a_SpecificShareable@shareable_outp_ls)
+A@c_SpecificProject@c_SpecificResults@a_SpecificShareable@shareable_outp_ls$paths_to_figs_ls <- specific::make_paths_to_ms_smry_plts_ls(paste0(A@d_TTUReports@a_TTUSynopsis@a_Ready4showPaths@outp_data_dir_1L_chr, "/Output"), A@c_SpecificProject@c_SpecificResults@a_SpecificShareable@shareable_outp_ls)
+A@d_TTUReports@a_TTUSynopsis@b_SpecificResults@a_SpecificShareable@shareable_outp_ls$results_ls$paths_to_figs_ls <- A@c_SpecificProject@c_SpecificResults@a_SpecificShareable@shareable_outp_ls$paths_to_figs_ls
+A@d_TTUReports@a_TTUSynopsis@authors_r3[1,6] <- "\"Orygen, Monash\"" 
+A@d_TTUReports@a_TTUSynopsis@authors_r3[1,9] <- "matthew.hamilton1@monash.edu" 
+A@d_TTUReports@a_TTUSynopsis@funding_1L_chr <- paste0(A@d_TTUReports@a_TTUSynopsis@funding_1L_chr %>% stringr::str_sub(end =-16), ", headspace and an Australian Government Research Training Program (RTP) Scholarship.")
 A@d_TTUReports@a_TTUSynopsis@abstract_args_ls$abstract_ls <- list(Background = "Quality Adjusted Life Years (QALYs) are often used in economic evaluations, yet utility weights for deriving them are rarely directly measured in mental health services.",
                                                                   Objective = "We aimed to identify the best mapping models and predictors for adolescent Assessment of Quality of Life - Six Dimensions (AQOL-6D) utility and assess the ability of mapping models to predict longitudinal change.", 
-                                                                  Methods = "We recruited 1107 young people attending Australian primary mental health services, collecting data at two time points, three months apart. Five linear and three generalised linear models were explored to identify the best mapping model. Ten-fold cross-validation using R^2, root mean square error (RMSE) and mean absolute error (MAE) were used to compare models and assess predictive ability of six candidate measures of psychological distress, depression and anxiety. Linear / generalised linear mixed effect models were used to construct longitudinal predictive models for AQoL-6D change.", 
+                                                                  Methods = "We recruited 1107 young people attending Australian primary mental health services, collecting data at two time points, three months apart. Five linear and three generalised linear models were explored to identify the best mapping model. Ten-fold cross-validation using R^2^, root mean square error (RMSE) and mean absolute error (MAE) were used to compare models and assess predictive ability of six candidate measures of psychological distress, depression and anxiety. Linear / generalised linear mixed effect models were used to construct longitudinal predictive models for AQoL-6D change.", 
                                                                   Results = "A depression measure (Patient Health Questionnaire-9) was the strongest independent predictor of health utility. Linear regression models with complementary log-log transformation of utility score were the best performing models. Between-person associations were slightly larger than within-person associations for most of the predictors.", 
                                                                   Conclusions = "Adolescent AQoL-6D utility can be derived from a range of psychological distress, depression and anxiety measures. Mapping models estimated from cross-sectional data can approximate longitudinal change but may slightly bias health utility predictions.",
                                                                   Data = "Replication code, detailed results and guidance on how to apply the models produced by this study are available in the online repository: https://doi.org/10.7910/DVN/DKDIB0.")
@@ -16,17 +19,20 @@ A@d_TTUReports@a_TTUSynopsis@title_1L_chr <- "Mapping psychological distress, de
 #dir.create(paste0(A@d_TTUReports@a_TTUSynopsis@a_Ready4showPaths@outp_data_dir_1L_chr,"/Markdown/Manuscript_Submission"))
 file.copy(list.files(getwd()),paste0(A@d_TTUReports@a_TTUSynopsis@a_Ready4showPaths@outp_data_dir_1L_chr,"/Markdown/Manuscript_Submission"), recursive = T, overwrite = T)
 authorSlot(A, "d_TTUReports", what_1L_chr = "Manuscript_Submission", download_tmpl_1L_lgl = F)
+# file.copy("Data/RefCopy.bib",paste0(A@d_TTUReports@a_TTUSynopsis@a_Ready4showPaths@outp_data_dir_1L_chr,"/Reports/Manuscript_Submission/Tex_Export"), overwrite = T)
+# Manually remove "../Data/", "../../../Output/" and "../../../Output/_Descriptives/" from Tex File
 authorReport(renewSlot(A,"d_TTUReports@a_TTUSynopsis@rmd_fl_nms_ls",
                        ready4show::make_rmd_fl_nms_ls(pdf_fl_nm_1L_chr = "TA_PDF")) %>% 
                procureSlot("d_TTUReports@a_TTUSynopsis"),
              fl_nm_1L_chr = "Supplement", what_1L_chr = "Manuscript_Submission")
-authorReport(renewSlot(A,"d_TTUReports@a_TTUSynopsis@rmd_fl_nms_ls",
-                       ready4show::make_rmd_fl_nms_ls()) %>% 
-               renewSlot("d_TTUReports@a_TTUSynopsis@outp_formats_chr",
-                         c("Word","Word")) %>%
-               procureSlot("d_TTUReports@a_TTUSynopsis"),
-             what_1L_chr = "Manuscript_Submission")
-
-
-
-##############################################setwd(paste0(A@d_TTUReports@a_TTUSynopsis@a_Ready4showPaths@outp_data_dir_1L_chr,"/Markdown/Manuscript_Submission/Parent_PDF"))
+#params_ls <- list(X = X)
+#params_ls$X@figures_in_body_lgl <- params_ls$X@tables_in_body_lgl <- F
+rmarkdown::render(paste0("Parent_",
+                         "Word",#
+                         "/",
+                         "Main_Word",#
+                         ".Rmd"),
+                  output_format = NULL,
+                  params = list(X = A@d_TTUReports@a_TTUSynopsis),
+                  output_file = "Manuscript.docx",
+                  output_dir = paste0(A@d_TTUReports@a_TTUSynopsis@a_Ready4showPaths@outp_data_dir_1L_chr,"/Reports/Manuscript_Submission"))
